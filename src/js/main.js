1e4e51147e3d88f588a72d4d5d2d988a02e37c12
Vladislav <flickervladislav@gmail.com>
2023-02-08 15:47:17 +0200

    fix: deploy

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import '../css/style.css'
 import javascriptLogo from '../img/javascript.svg'
+import viteLogo from '../img/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src=${viteLogo} class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
